ce53a7c2c5fe03d91f131fe5ed9fdfa2c8dbee59
Amit32624 <120220153@umail.ucc.ie>
2021-10-16 13:26:01 +0100

    mlflow incorporated

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,19 +1,28 @@
 
 import pytest
 
+from prediction_service.prediction import form_response,api_response
+import prediction_service
 
 
 
-class NotInRange(Exception):
-    def __init__(self,message="Value not in range"):
-        # self.input =input_
-        self.message = message
-        super().__init__(self.message)
+
+
+
+
+
+
+
+# class NotInRange(Exception):
+#     def __init__(self,message="Value not in range"):
+#         # self.input =input_
+#         self.message = message
+#         super().__init__(self.message)
         
         
-def test_generic():
-    a = 2
-    with pytest.raises(NotInRange):
-        if a not in range(10,20):
-            raise NotInRange
+# def test_generic():
+#     a = 2
+#     with pytest.raises(NotInRange):
+#         if a not in range(10,20):
+#             raise NotInRange
 
